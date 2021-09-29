bcccf0b7c9aa30689ea490eebd91656e09f14198
John Holbrook <john@johnholbrook.us>
2021-09-29 16:17:04 -0400

    Add command-line flags, update readme

--- README.md
+++ README.md
@@ -5,3 +5,25 @@ Python script for batch-rendering images of individual LDraw parts from CSV inpu
 Requires LeoCAD to be isntalled and accessible from the command line – in macOS, I did this by adding `/Applications/LeoCAD.app/Contents/MacOS` to my `$PATH`.
 
 ## Example usage
+```
+$ python3 generate-images.py --csv-template
+$ cat template.csv
+File Name,Number,Color Code,Latitude,Longitude
+```
+
+```
+$ python3 generate_images.py -i super-kit-tray.csv -o images
+```
+
+```
+$ python3 generate_images.py --help
+usage: generate_images.py [-h] [-i input.csv] [-o path/to/dir] [--csv-template]
+
+Batch-generate images of LDraw parts from a CSV file.
+
+optional arguments:
+  -h, --help      show this help message and exit
+  -i input.csv    Input CSV file
+  -o path/to/dir  Output directory (will be created if it doesn't exist)
+  --csv-template  Output a blank CSV file of the correct format to template.csv
+```
--- generate_images.py
+++ generate_images.py
@@ -1,12 +1,41 @@
-import csv, subprocess
+import csv, subprocess, argparse, time
+from os import path
+from sys import stderr
 
-subprocess.run("mkdir images", shell=True)
-subprocess.run("mkdir ldr_files", shell=True)
+# configure argument parser
+parser = argparse.ArgumentParser(description="Batch-generate images of LDraw parts from a CSV file.")
+parser.add_argument("-i", help="Input CSV file", default="", metavar="input.csv")
+parser.add_argument("-o", help="Output directory (will be created if it doesn't exist)", default="output", metavar="path/to/dir")
+parser.add_argument("--csv-template", help="Output a blank CSV file of the correct format to template.csv", action="count")
+args = parser.parse_args()
 
-parts = []
+# check if the --csv-template flag was included
+if args.csv_template != None:
+    header="File Name,Number,Color Code,Latitude,Longitude\n"
+    if not path.exists("template.csv"):
+        with open("template.csv", "w") as of:
+            of.write(header)
+            of.close()
+    else:
+        stderr.write("ERROR: template.csv already exists.\n")
+    exit()
+
+if args.i == "":
+    stderr.write("ERROR: Must specify input file.\n")
+    exit()
+
+#create the directory for temporary ldraw files
+tmp_dir_name = "ldr_files"
+while path.exists(tmp_dir_name):
+    tmp_dir_name += "_tmp"
+subprocess.run(f"mkdir {tmp_dir_name}", shell=True)
+
+# create the output directory
+subprocess.run(f"mkdir {args.o}", shell=True)
 
 # open and parse the CSV file
-with open("partslist.csv") as csvfile:
+parts = []
+with open(args.i) as csvfile:
     reader = csv.DictReader(csvfile)
     for part in reader:
         parts.append(part)
@@ -15,13 +44,23 @@ for part in parts:
     pn = part["Number"]
     fn = part["File Name"]
     color = part["Color Code"]
+    lat = part["Latitude"]
+    lon = part["Longitude"]
 
-    # create a temp.ldr file which has the part in it (required to make leocad zoom extents work properly)
-    with open(f"ldr_files/{pn}.ldr", "w") as tmpldr:
+    # default values for color, lat, lon
+    if color == "": color = 0
+    if lat == "": lat = 0
+    if lon == "": lon = 0
+    print(color, lat, lon)
+
+    # create a temporary ldr file which has the part in it (required to make leocad zoom extents work properly)
+    with open(f"{tmp_dir_name}/{pn}.ldr", "w") as tmpldr:
         tmpldr.write(f"1 {color} 0 0 0 1 0 0 0 1 0 0 0 1 {fn}")
         tmpldr.close()
     
     # generate the image for this part
-    # subprocess.run(f"LeoCAD -i output/{pn}.png -w 1000 -h 1000 --stud-style 6 --line-width 5 --edge-color '#FF000000' tmp/{pn}.ldr", shell=True)
-    subprocess.run(f"LeoCAD -i images/{pn}.png -w 1000 -h 1000 ldr_files/{pn}.ldr", shell=True)
-        
+    subprocess.run(f"LeoCAD -i {args.o}/{pn}.png -w 1000 -h 1000 --camera-angles {lat} {lon} ldr_files/{pn}.ldr", shell=True)
+
+# time.sleep(5)
+# delete the temporary ldraw files
+subprocess.run(f"rm -r {tmp_dir_name}", shell=True)
